6f6bc94c58a9fa6242bf2fd41276a8fe17f4e5d7
Daniel Bright <brahyt@gmail.com>
2019-09-18 09:05:01 -0700

    changed objectdrill

--- objectDrills1.js
+++ objectDrills1.js
@@ -1,8 +1,12 @@
-const createMyObject = {
-  foo: 'bar',
-  answerToUniverse: 42,
-  'olly olly': 'oxen free',
-  sayHello: function(){
-    return 'hello'
+function createMyObject(obj){
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello'
+    }
   }
 }
+
+createMyObject('test')
