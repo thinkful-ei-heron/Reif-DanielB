b1ae991b19b6567c6746f66f11da04319b4483f5
Bridger <bridgerrhammond@gmail.com>
2019-09-18 17:43:10 -0500

    hero store

--- avengers.js
+++ avengers.js
@@ -67,8 +67,45 @@ function findOne(arr, query) {
   return console.log('null')
 }
 
-findOne(HEROES, { id: 2 })
-findOne(HEROES, { id: 5, squad: 'Justice League' })
-findOne(HEROES, { squad: 'Justice League' })
-findOne(HEROES, { id: 100 })
-findOne(HEROES, { id: 2, name: 'Aquaman' })
+// findOne(HEROES, { id: 2 })
+// findOne(HEROES, { id: 5, squad: 'Justice League' })
+// findOne(HEROES, { squad: 'Justice League' })
+// findOne(HEROES, { id: 100 })
+// findOne(HEROES, { id: 2, name: 'Aquaman' })
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne(query) {
+    let arr = this.store.heroes;
+    let possible;
+    let solution;
+    for (let key in query) {
+      for (let i = 0; i < arr.length; i++) {
+        possible = arr.filter(e => e[key] === query[key]);
+        //      console.log(possible)
+      }
+      if (solution === undefined) solution = possible;
+      if (solution[0] === undefined) return console.log('null');
+    }
+    for (let key in solution) {
+      for (let i = 0; i < possible.length; i++) {
+        if (solution[0] === possible[i]) {
+          return console.log(solution[0])
+        }
+      }
+    }
+    return console.log('null');
+  }
+};
+
+Database.findOne({ id: 2, name: 'Aquaman' });
