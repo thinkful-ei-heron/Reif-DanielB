a0aa677de38d3d949a77380d37bc42f3782d4109
Bridger <bridgerrhammond@gmail.com>
2019-09-18 12:53:41 -0500

    hobbit stuff

--- objInit.js
+++ objInit.js
@@ -12,8 +12,11 @@ loaf.hydration = function () {
 
 console.log(loaf.hydration());
 
-// Create an object called loaf using an object initializer({}) with two properties: flour, which should be set to 300 and water which should be set to 210.
-// Use console.log to print the flour and water properties.
-// Add an empty method to the loaf object called hydration.
-// Fill in the body of the method to return the hydration of the loaf(the water divided by the flour multiplied by 100).
-// Call the hydration method and use console.log to print the result.
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+const hobbitFood = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+console.log(hobbitFood.meals[3]);
