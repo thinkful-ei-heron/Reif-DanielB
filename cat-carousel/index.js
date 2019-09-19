b5c14c96dc192ea34177d524f0ca101bd3fcdcdd
Bridger <bridgerrhammond@gmail.com>
2019-09-19 12:27:40 -0500

    changed to .keydown instead of .on(keypress

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 $('.thumbnail').on('click', e => {
-  let clicked = e.target
+  let clicked = e.target;
   $('.hero img').attr('src', clicked.src).attr('alt', clicked.alt);
 });
 
-$('.thumbnail').on('keypress', e => {
-  if(e.keyCode === 13){
-    let clicked = e.target
+$('.thumbnail').keydown(e => {
+  if (e.keyCode === 13) {
+    let clicked = e.target;
     $('.hero img').attr('src', clicked.firstChild.src).attr('alt', clicked.firstChild.src);
   }
 });
