474e73e96ce657a66ba7c005013e4205e619b9d7
Daniel Bright <brahyt@gmail.com>
2019-09-19 09:44:17 -0700

    cats

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -2,3 +2,15 @@ $('.thumbnail').on('click', e => {
   console.log(e.target);
 });
 
+$('.thumbnail').on('click', e => {
+  let clicked = document.activeElement
+  $('.hero img').attr('src', clicked.src);
+  $('.hero img').attr('alt', clicked.alt)
+});
+
+//$('.thumbnail').keypress(e => {
+//  let clicked = $('document.activeElement')
+//  console.log(clicked)
+//  $('.hero img').attr('src', clicked.src);
+//  $('.hero img').attr('alt', clicked.alt)
+//});
