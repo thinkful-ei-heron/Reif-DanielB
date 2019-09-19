e75b6539fcf8c087c71ad68bbcac31dbe9a11278
Bridger <bridgerrhammond@gmail.com>
2019-09-19 10:57:44 -0500

    fizzbuzz

---
+++ fizzBuzz/app.js
@@ -0,0 +1,32 @@
+'use strict';
+// your code here
+
+$(function () {
+  $('#number-chooser').submit(event => {
+    event.stopPropagation();
+    event.preventDefault();
+    let results = '';
+    let countTo = $('#number-choice').val();
+    function fizzBuzz(counter) {
+      for (let i = 1; i <= counter; i++) {
+        switch (true) {
+          case i % 3 === 0 && i % 5 === 0:
+            results += '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span ></div >';
+            break;
+          case i % 3 === 0:
+            results += '<div class="fizz-buzz-item fizz"><span>fizz</span ></div >';
+            break;
+          case i % 5 === 0:
+            results += '<div class="fizz-buzz-item buzz"><span>buzz</span ></div >';
+            break;
+          default:
+            results += `<div class="fizz-buzz-item"><span>${i}</span ></div >`;
+        }
+      }
+      // console.log(results);
+      return results;
+    }
+    fizzBuzz(countTo);
+    $('.js-results').append(results);
+  });
+});
