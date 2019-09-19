e209cdf3ce5b3bc2fc1cb86f9d90100370935456
Bridger <bridgerrhammond@gmail.com>
2019-09-19 12:39:05 -0500

    formatting

--- cat-carousel/index.js
+++ cat-carousel/index.js
@@ -1,12 +1,14 @@
-'use strict';
+'use strict'
 $('.thumbnail').on('click', e => {
-  let clicked = e.target;
-  $('.hero img').attr('src', clicked.src).attr('alt', clicked.alt);
+  let clicked = e.target
+  $('.hero img').attr('src', clicked.src).attr('src', clicked.src);
+  $('.hero img').attr('alt', clicked.alt).attr('alt', clicked.alt);
 });
 
 $('.thumbnail').keydown(e => {
   if (e.keyCode === 13) {
     let clicked = e.target;
-    $('.hero img').attr('src', clicked.firstChild.src).attr('alt', clicked.firstChild.src);
+    $('.hero img').attr('alt', clicked.firstChild.alt).attr('alt', clicked.firstChild.alt);
+    $('.hero img').attr('src', clicked.firstChild.src).attr('src', clicked.firstChild.src);
   }
 });
